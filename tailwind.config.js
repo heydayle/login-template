35091b971fd0d9e276ef48bd82180fc0b5ae663b
thinh.le <hungthinh.ckc@gmail.com>
2024-06-10 11:34:48 +0700

    style: update left side

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    fontSize: {
+      xs: '0.8rem',
+      sm: '0.875rem',
+      base: '1rem',
+      xl: '1.25rem',
+      heading: '2rem',
+    }
   },
   plugins: [],
 }
