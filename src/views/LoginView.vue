3ec490778f723bd8e758f6e0664edbc69fae1bdb
thinh.le <thinhldh@ycomm.vn>
2024-06-10 09:30:17 +0700

    init

---
+++ src/views/LoginView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const Benefits = [
+  {
+    icon: '',
+    title: 'Spot downtime and help you debug',
+    description: 'We spot downtime and help you debug, and resolve problems in the fastest time. Save efforts and lower the risk of downtime.'
+  },
+  {
+    icon: '',
+    title: 'Spot and resolve downtime faster',
+    description: 'Monitor every aspect of your website from anywhere globally, immediate alerts, automatic escalation and resolve issues quickly.'
+  },
+  {
+    icon: '',
+    title: 'Uptime monitoring',
+    description: 'Lorem Ipsum placeholder text for use in your graphic, print and web layouts, and discover plugins for your favorite writing, design and blogging'
+  },
+  {
+    icon: '',
+    title: 'Incident management',
+    description: 'Lorem Ipsum placeholder text for use in your graphic, print and web layouts, and discover plugins for your favorite writing, design and blogging'
+  },
+]
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>Downtime happens? No problem!
+        Get 100 monitors within 5 minutes for free</h1>
+      <div>
+
+      </div>
+    </div>
+    <div>Form</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
